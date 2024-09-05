e81510ed67e16ed5237f9f1c260b8d90aea19754
Rivea108 <zkdhtm152@naver.com>
2024-09-05 17:40:48 +0900

    mini2

--- src/main/java/txtBoard/post/PostController.java
+++ src/main/java/txtBoard/post/PostController.java
@@ -1,24 +1,18 @@
-package txtBoard;
-
-import txtBoard.post.Post;
-import txtBoard.post.PostRepository;
-import txtBoard.post.PostView;
+package txtBoard.post;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class BoardApp {
+public class PostController {
 
+    private PostRepository postRepository = new PostRepository();
+    private PostView postView = new PostView();
+    private Scanner sc = new Scanner(System.in);
+    private int lastestId = 4;
 
-    Scanner sc = new Scanner(System.in);
-    int lastestId = 4;
-    PostRepository postRepository = new PostRepository();
-    PostView postView = new PostView();
-
-    // 값의 초기화는 대부분 생성자에서 해주는 것을 권장합니다. 다양한 로직 수행 가능합니다.
-    public BoardApp() {
+    public PostController() {
 
         Post p1 = new Post(1, "안녕하세요 반갑습니다. java 공부중이에요.", "냉무", getCurrentDateTime(), 0);
         Post p2 = new Post(2, "java 질문좀 할게요~.", "냉무", getCurrentDateTime(), 0);
@@ -29,35 +23,34 @@ public class BoardApp {
         postRepository.save(p3);
     }
 
-    public void run() {
-        while (true) {
-            System.out.print("명령어 : ");
-            String command = sc.nextLine();
-            if (command.equals("exit")) {
-                System.out.println("프로그램을 종료합니다.");
-                break;
-            } else if (command.equals("add")) {
-                add();
-            } else if (command.equals("list")) {
-                list();
-            } else if (command.equals("update")) {
-                update();
-            } else if (command.equals("delete")) {
-                delete();
-            } else if (command.equals("detail")) {
-                detail();
-            } else if (command.equals("search")) {
-                search();
-            }
+    // command : detail
+    public void detail() {
+
+        System.out.print("상세보기 할 게시물 번호 : ");
+        int targetId = Integer.parseInt(sc.nextLine());//첫빠따로 시작!!!!!!!!!
+        //System.out.println("=====" + post.getId() + "번 게시글 =====");
+
+        Post post = postRepository.findPostById(targetId);  // 창고에서 꺼내서
+
+        // ==== 요리를 한다음
+        if (post == null) {
+            System.out.println("없는 게시물 번호 입니다.");
+            return;
         }
+        post.increaseHit();
+        // ======
+        postView.printPostDetail(post); // 서빙한테 넘겨줌
+
     }
 
-    private void list() {
+    // command : list
+    public void list() {
         ArrayList<Post> posts = postRepository.getPosts();
         postView.printPostList(posts);
     }
 
-    private void search() {
+    // command : search
+    public void search() {
         System.out.print("검색 키워드 : ");
         String keyword = sc.nextLine();
 
@@ -65,43 +58,8 @@ public class BoardApp {
         postView.printPostList(searchedPostList);
     }
 
-    private void detail() {
-        System.out.print("상세보기 할 게시물 번호 : ");
-        int targetId = Integer.parseInt(sc.nextLine());
-
-        Post post = postRepository.findPostById(targetId);
-
-        if (post == null) {
-            System.out.println("없는 게시물 번호 입니다.");
-            return;
-        }
-        post.increaseHit();
-        postView.printPostDetail(post);
-
-
-        int function = Integer.parseInt(sc.nextLine());//2빠따로 시작!!!!!!!!!!!!!
-        if (function == 1) {
-            System.out.println("상세보기 기능을 선택해주세요(1. 댓글 등록, 2. 추천, 3. 수정, 4.삭제, 5.목록으로) : ");
-            int number = Integer.parseInt(sc.nextLine());
-            System.out.println("댓글 내용 : ");
-            String comment = sc.nextLine();
-            if (function == 2) {
-                System.out.println("추천");
-            }
-            if (function == 3) {
-                System.out.println("수정");
-            }
-            if (function == 4) {
-                System.out.println("삭제");
-            }
-            if (function == 5) {
-                System.out.println("상세보기 화면을 빠져나갑니다.");
-                return;
-            }
-        }
-    }
-
-    private void delete() {
+    // command : delete
+    public void delete() {
         System.out.print("삭제할 게시물 번호 : ");
         int targetId = Integer.parseInt(sc.nextLine());
         Post post = postRepository.findPostById(targetId);
@@ -116,7 +74,8 @@ public class BoardApp {
 
     }
 
-    private void update() {
+    // command : update
+    public void update() {
         System.out.print("수정할 게시물 번호 : ");
         int targetId = Integer.parseInt(sc.nextLine());
 
@@ -137,7 +96,8 @@ public class BoardApp {
         System.out.println("수정이 완료되었습니다.");
     }
 
-    private void add() {
+    // command : add
+    public void add() {
         System.out.print("게시물 제목을 입력해주세요 : ");
         String title = sc.nextLine();
         System.out.print("게시물 내용을 입력해주세요 : ");
@@ -147,7 +107,6 @@ public class BoardApp {
         System.out.println("게시물이 등록되었습니다.");
         lastestId++;
     }
-
 
     public String getCurrentDateTime() {
 
